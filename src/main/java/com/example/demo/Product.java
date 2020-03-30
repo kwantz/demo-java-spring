838d2a4d8b38d3c870e5efcff81660bebbf14e10
kwantz <erickkwantantz123@gmail.com>
2020-03-30 17:37:12 +0700

    Create simple RESTful Product Service

---
+++ src/main/java/com/example/demo/Product.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class Product {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName() {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/demo/ProductServiceController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ProductServiceController {
+
+    private static Map<Integer, Product> productRepo = new HashMap<>();
+
+    @RequestMapping(value = "/products")
+    public ResponseEntity<Object> getProducts() {
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+        productRepo.put(product.getId(), product);
+        return new ResponseEntity<>(productRepo.get(product.getId()), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateProduct(@RequestBody Product product, @PathVariable("id") Integer id) {
+        productRepo.remove(id);
+        productRepo.put(id, product);
+        return new ResponseEntity<>(productRepo.get(product.getId()), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> delteProduct(@PathVariable("id") Integer id) {
+        productRepo.remove(id);
+        return new ResponseEntity<>("Product is deleted successsfully", HttpStatus.OK);
+    }
+}
